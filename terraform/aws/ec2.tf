bce4e874bd22900708d73775a4e6e95bd0808c35
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2021-11-04 16:35:16 -0700

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_host" {
   # ec2 have plain text secrets in user data
-  ami           = "${var.ami}"
+  ami           = "${var.ami}" 
   instance_type = "t2.nano"
 
   vpc_security_group_ids = [
@@ -33,7 +33,7 @@ EOF
 
 resource "aws_ebs_volume" "web_host_storage" {
   # unencrypted volume
-  availability_zone = "${var.region}a"
+  availability_zone = "${var.region}a" 
   #encrypted         = false  # Setting this causes the volume to be recreated on apply 
   size = 1
   tags = merge({
@@ -52,7 +52,7 @@ resource "aws_ebs_volume" "web_host_storage" {
 
 resource "aws_ebs_snapshot" "example_snapshot" {
   # ebs snapshot without encryption
-  volume_id   = "${aws_ebs_volume.web_host_storage.id}"
+  volume_id   = "${aws_ebs_volume.web_host_storage.id}" 
   description = "${local.resource_prefix.value}-ebs-snapshot"
   tags = merge({
     Name = "${local.resource_prefix.value}-ebs-snapshot"
@@ -69,14 +69,14 @@ resource "aws_ebs_snapshot" "example_snapshot" {
 }
 
 resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
+  device_name = "/dev/sdh" 
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
   instance_id = "${aws_instance.web_host.id}"
 }
 
 resource "aws_security_group" "web-node" {
   # security group is open to the world in SSH port
-  name        = "${local.resource_prefix.value}-sg"
+  name        = "${local.resource_prefix.value}-sg" 
   description = "${local.resource_prefix.value} Security Group"
   vpc_id      = aws_vpc.web_vpc.id
 
@@ -115,7 +115,7 @@ resource "aws_security_group" "web-node" {
 }
 
 resource "aws_vpc" "web_vpc" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = "172.16.0.0/16" 
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = merge({
@@ -133,7 +133,7 @@ resource "aws_vpc" "web_vpc" {
 }
 
 resource "aws_subnet" "web_subnet" {
-  vpc_id                  = aws_vpc.web_vpc.id
+  vpc_id                  = aws_vpc.web_vpc.id 
   cidr_block              = "172.16.10.0/24"
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
@@ -153,7 +153,7 @@ resource "aws_subnet" "web_subnet" {
 }
 
 resource "aws_subnet" "web_subnet2" {
-  vpc_id                  = aws_vpc.web_vpc.id
+  vpc_id                  = aws_vpc.web_vpc.id 
   cidr_block              = "172.16.11.0/24"
   availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
@@ -174,7 +174,7 @@ resource "aws_subnet" "web_subnet2" {
 
 
 resource "aws_internet_gateway" "web_igw" {
-  vpc_id = aws_vpc.web_vpc.id
+  vpc_id = aws_vpc.web_vpc.id 
 
   tags = merge({
     Name = "${local.resource_prefix.value}-igw"
@@ -191,7 +191,7 @@ resource "aws_internet_gateway" "web_igw" {
 }
 
 resource "aws_route_table" "web_rtb" {
-  vpc_id = aws_vpc.web_vpc.id
+  vpc_id = aws_vpc.web_vpc.id 
 
   tags = merge({
     Name = "${local.resource_prefix.value}-rtb"
@@ -208,17 +208,17 @@ resource "aws_route_table" "web_rtb" {
 }
 
 resource "aws_route_table_association" "rtbassoc" {
-  subnet_id      = aws_subnet.web_subnet.id
+  subnet_id      = aws_subnet.web_subnet.id 
   route_table_id = aws_route_table.web_rtb.id
 }
 
 resource "aws_route_table_association" "rtbassoc2" {
-  subnet_id      = aws_subnet.web_subnet2.id
+  subnet_id      = aws_subnet.web_subnet2.id 
   route_table_id = aws_route_table.web_rtb.id
 }
 
 resource "aws_route" "public_internet_gateway" {
-  route_table_id         = aws_route_table.web_rtb.id
+  route_table_id         = aws_route_table.web_rtb.id 
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.web_igw.id
 
@@ -229,7 +229,7 @@ resource "aws_route" "public_internet_gateway" {
 
 
 resource "aws_network_interface" "web-eni" {
-  subnet_id   = aws_subnet.web_subnet.id
+  subnet_id   = aws_subnet.web_subnet.id 
   private_ips = ["172.16.10.100"]
 
   tags = merge({
@@ -248,7 +248,7 @@ resource "aws_network_interface" "web-eni" {
 
 # VPC Flow Logs to S3
 resource "aws_flow_log" "vpcflowlogs" {
-  log_destination      = aws_s3_bucket.flowbucket.arn
+  log_destination      = aws_s3_bucket.flowbucket.arn 
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.web_vpc.id
@@ -269,7 +269,7 @@ resource "aws_flow_log" "vpcflowlogs" {
 }
 
 resource "aws_s3_bucket" "flowbucket" {
-  bucket        = "${local.resource_prefix.value}-flowlogs"
+  bucket        = "${local.resource_prefix.value}-flowlogs" 
   force_destroy = true
 
   tags = merge({
@@ -288,21 +288,21 @@ resource "aws_s3_bucket" "flowbucket" {
 }
 
 output "ec2_public_dns" {
-  description = "Web Host Public DNS name"
+  description = "Web Host Public DNS name" 
   value       = aws_instance.web_host.public_dns
 }
 
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "The ID of the VPC" 
   value       = aws_vpc.web_vpc.id
 }
 
 output "public_subnet" {
-  description = "The ID of the Public subnet"
+  description = "The ID of the Public subnet" 
   value       = aws_subnet.web_subnet.id
 }
 
 output "public_subnet2" {
-  description = "The ID of the Public subnet"
+  description = "The ID of the Public subnet" 
   value       = aws_subnet.web_subnet2.id
 }
