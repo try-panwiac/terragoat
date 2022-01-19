a8419034dcda6ce0b338162fbf256d2b2cacb4b5
taysmith-test <87036659+taysmith-test@users.noreply.github.com>
2022-01-18 22:43:54 -0800

    Update storage.tf

--- terraform/simple_instance/storage.tf
+++ terraform/simple_instance/storage.tf
@@ -2,21 +2,19 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+}
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_resource_group" "trekgroup" {
+  name     = "trekgroup-resources"
+  location = "West US"
+}
+
+resource "azurerm_storage_account" "storagebay" {
   name                     = "storageaccountname"
-  resource_group_name      = "resourcegroup"
-  location                 = "West US"
+  resource_group_name      = azurerm_resource_group.trekgroup.name
+  location                 = azurerm_resource_group.trekgroup.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    git_commit           = "1d0149bb6f635744b060c574d4baf6c9eeda5e11"
-    git_file             = "terraform/simple_instance/storage.tf"
-    git_last_modified_at = "2022-01-19 06:35:25"
-    git_last_modified_by = "87036659+taysmith-test@users.noreply.github.com"
-    git_modifiers        = "87036659+taysmith-test"
-    git_org              = "taysmith-test"
-    git_repo             = "terragoat"
-    yor_trace            = "0a2f7aa8-699c-4129-b2ab-3129edfee232"
-  }
+  account_replication_type = "GRS"
 }
