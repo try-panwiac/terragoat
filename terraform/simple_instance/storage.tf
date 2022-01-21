c741c18ffcf520b736d62981c5c50b062c1fc683
taysmith-test <87036659+taysmith-test@users.noreply.github.com>
2022-01-21 13:12:09 -0800

    Update storage.tf

--- terraform/simple_instance/storage.tf
+++ terraform/simple_instance/storage.tf
@@ -43,3 +43,9 @@ resource "azurerm_storage_account" "storagebay" {
     yor_trace            = "faaf4f2b-e42a-4237-bf6d-ee1b6d5b66fe"
   }
 }
+
+resource "azurerm_storage_container" "storagebay10" {
+  name                  = "storagebay10${random_string.sa_name_affix.result}"
+  storage_account_name  = azurerm_storage_account.storagebay.name
+  container_access_type = "blob"
+}
