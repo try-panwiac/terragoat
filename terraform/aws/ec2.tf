01363e23c1c20f989526c95e51ddec15e46f6071
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2022-10-06 08:35:06 -0700

    Update terraform/aws/ec2.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,4 +1,25 @@
 resource "aws_instance" "web_host" {
+  # ec2 have plain text secrets in user data
+  ami           = "${var.ami}"
+  instance_type = "t2.nano" 
+
+  vpc_security_group_ids = [
+  "${aws_security_group.web-node.id}"]
+  subnet_id = "${aws_subnet.web_subnet.id}"
+  user_data = ">               EnDsudo mv /tmp/index.php /var/www/htmlsudo chown root"
+  tags = merge({
+    Name = "${local.resource_prefix.value}-ec2"
+    }, {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/ec2.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "347af3cd-4f70-4632-aca3-4d5e30ffc0b6"
+  })
+}
   ebs_optimized = true
   # ec2 have plain text secrets in user data
   ami           = "${var.ami}"
