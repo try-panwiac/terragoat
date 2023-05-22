7d3e079d9533244741355569daaf22a3ff35e689
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2023-05-22 09:29:52 -0700

    New App2

---
+++ new-app/ec2.tf
@@ -0,0 +1,103 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami             = "encrypted_ami_id"
+  instance_type   = "t2.micro"
+  key_name        = "example_keypair"
+  subnet_id       = "example_subnet_id"
+  vpc_security_group_ids = ["example_security_group_id"]
+  associate_public_ip_address = false
+
+  iam_instance_profile {
+    name = "example"
+  }
+
+  root_block_device {
+    encrypted     = true
+  }
+
+  launch_template {
+    id      = aws_launch_template.example.id
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+  ebs_optimized = true
+}
+
+
+resource "aws_launch_template" "example" {
+  name = "example"
+
+  user_data = <<EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup python -m SimpleHTTPServer 80 &
+              export access_key = "AKIAIOSFODNN7EXAMAAA"
+              export secret_key = "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"
+              EOF
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 10
+    encrypted = true
+  }
+
+  ebs_block_device {
+    device_name = "/dev/xvdf"
+    volume_type = "gp2"
+    volume_size = 10
+    encrypted = true
+  }
+
+  iam_instance_profile {
+    name = "example"
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  image_id = "encrypted_ami_id"
+  instance_type = "t2.micro"
+}
+
+
+
+
+
+###Delete
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
