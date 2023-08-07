714c9c76cc5d2ddf141e01f903f29bcdcd2198c7
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2023-08-06 21:26:27 -0700

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_host" {
   # ec2 have plain text secrets in user data
-  ami           = "${var.ami}"
+  ami           = "${var.ami}" 
   instance_type = "t2.nano"
 
   vpc_security_group_ids = [
@@ -34,7 +34,7 @@ EOF
 resource "aws_ebs_volume" "web_host_storage" {
   # unencrypted volume
 
-  availability_zone = "${var.region}a"
+  availability_zone = "${var.region}a" 
   #encrypted         = false  # Setting this causes the volume to be recreated on apply 
   size = 1
   tags = merge({
@@ -53,7 +53,7 @@ resource "aws_ebs_volume" "web_host_storage" {
 
 resource "aws_ebs_snapshot" "example_snapshot" {
   # ebs snapshot without encryption
-  volume_id   = "${aws_ebs_volume.web_host_storage.id}"
+  volume_id   = "${aws_ebs_volume.web_host_storage.id}" 
   description = "${local.resource_prefix.value}-ebs-snapshot"
   tags = merge({
     Name = "${local.resource_prefix.value}-ebs-snapshot"
@@ -71,13 +71,13 @@ resource "aws_ebs_snapshot" "example_snapshot" {
 
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.web_host_storage.id}"
+  volume_id   = "${aws_ebs_volume.web_host_storage.id}" 
   instance_id = "${aws_instance.web_host.id}"
 }
 
 resource "aws_security_group" "web-node" {
   # security group is open to the world in SSH port
-  name        = "${local.resource_prefix.value}-sg"
+  name        = "${local.resource_prefix.value}-sg" 
   description = "${local.resource_prefix.value} Security Group"
   vpc_id      = aws_vpc.web_vpc.id
 
@@ -116,7 +116,7 @@ resource "aws_security_group" "web-node" {
 }
 
 resource "aws_vpc" "web_vpc" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = "172.16.0.0/16" 
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = merge({
@@ -134,7 +134,7 @@ resource "aws_vpc" "web_vpc" {
 }
 
 resource "aws_subnet" "web_subnet" {
-  vpc_id                  = aws_vpc.web_vpc.id
+  vpc_id                  = aws_vpc.web_vpc.id 
   cidr_block              = "172.16.10.0/24"
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
@@ -154,7 +154,7 @@ resource "aws_subnet" "web_subnet" {
 }
 
 resource "aws_subnet" "web_subnet2" {
-  vpc_id                  = aws_vpc.web_vpc.id
+  vpc_id                  = aws_vpc.web_vpc.id 
   cidr_block              = "172.16.11.0/24"
   availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
@@ -175,7 +175,7 @@ resource "aws_subnet" "web_subnet2" {
 
 
 resource "aws_internet_gateway" "web_igw" {
-  vpc_id = aws_vpc.web_vpc.id
+  vpc_id = aws_vpc.web_vpc.id 
 
   tags = merge({
     Name = "${local.resource_prefix.value}-igw"
@@ -192,7 +192,7 @@ resource "aws_internet_gateway" "web_igw" {
 }
 
 resource "aws_route_table" "web_rtb" {
-  vpc_id = aws_vpc.web_vpc.id
+  vpc_id = aws_vpc.web_vpc.id 
 
   tags = merge({
     Name = "${local.resource_prefix.value}-rtb"
