1414bfe861d7c29a2cd376796fb8ef5fa6ef62cc
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2023-08-16 11:39:45 -0700

    Update key_vault.tf

--- terraform/azure/key_vault.tf
+++ terraform/azure/key_vault.tf
@@ -54,19 +54,3 @@ resource "azurerm_key_vault_key" "generated" {
     yor_trace            = "afbc6e13-63d9-4e6c-8914-d58b7744b5dd"
   }
 }
-
-resource "azurerm_key_vault_secret" "secret" {
-  key_vault_id = azurerm_key_vault.example.id
-  name         = "terragoat-secret-${var.environment}"
-  value        = random_string.password.result
-  tags = {
-    git_commit           = "f8ff847bb69370bbe03b3d2b70db586ff6c867fc"
-    git_file             = "terraform/azure/key_vault.tf"
-    git_last_modified_at = "2020-06-19 21:16:08"
-    git_last_modified_by = "Adin.Ermie@outlook.com"
-    git_modifiers        = "Adin.Ermie/nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "40517524-f05d-485b-bfbe-3fa0dbee511e"
-  }
-}
