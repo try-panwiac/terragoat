2ad44e2206413939df04f84a6d10444cbedd2070
panwjoe <99195980+panwjoe@users.noreply.github.com>
2023-12-15 08:57:32 -0500

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -31,7 +31,29 @@ EOF
   })
 }
 
+
+
 resource "aws_ebs_volume" "web_host_storage" {
+  # unencrypted volume
+
+  availability_zone = "${var.region}a"
+  #encrypted         = false  # Setting this causes the volume to be recreated on apply 
+  size = 1
+  tags = merge({
+    Name = "${local.resource_prefix.value}-ebs"
+    }, {
+    git_commit           = "d3439f0f2af62f6fa3521e14d6c27819ef8f12e1"
+    git_file             = "terraform/aws/ec2.tf"
+    git_last_modified_at = "2021-05-02 11:17:26"
+    git_last_modified_by = "nimrodkor@users.noreply.github.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "try-panwiac"
+    git_repo             = "terragoat"
+    yor_trace            = "c5509daf-10f0-46af-9e03-41989212521d"
+  })
+}
+
+resource "aws_ebs_volume" "web_host_storage2" {
   # unencrypted volume
 
   availability_zone = "${var.region}a"
