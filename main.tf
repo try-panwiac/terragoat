31c7275fbc3f07438cba862a6c787aff083436c6
Taylor <28880387+tsmithv11@users.noreply.github.com>
2025-01-30 10:52:17 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_storage_account" "example" {
+  name                     = "foo"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  queue_properties {
+    logging {
+      delete                = false
+      read                  = false
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+  
+  allow_nested_items_to_be_public = true
+}
