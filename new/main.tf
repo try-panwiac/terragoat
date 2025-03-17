8e4d5ba3a50961efb2e43dcbee0b2823ba8d520a
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2025-03-17 09:10:41 -0700

    Create main.tf

---
+++ new/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "my-tf-test-bucket-"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Access_Key  = "AKIAIOSFODNN7EXAMAAA"
+  }
+}
