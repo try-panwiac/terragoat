8468c28a7363ea39020cff07cd6e9a0a8cbf42ac
Taylor <28880387+tsmithv11@users.noreply.github.com>
2025-05-19 10:24:16 -0700

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -27,7 +27,7 @@ EOF
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
-    yor_trace            = "347af3cd-4f70-4632-aca3-4d5e30ffc0b6"
+    yor_trace            = "123abc"
   })
 }
 
